300941bd4f34d0386f3172dfdcd46ae93f1ccfe0
fakeboom <fakeboom@foxmail.com>
2020-04-04 17:57:39 +0800

    nn

--- fork.c
+++ fork.c
@@ -9,14 +9,12 @@ int main ()
         printf("error in fork!");   
     else if (fpid == 0) {  
         printf("i am the child process, my process id is %d\n",getpid());   
-        printf("我是爹的儿子/n");//对某些人来说中文看着更直白。  
         count++;  
     }  
     else {  
         printf("i am the parent process, my process id is %d\n",getpid());   
-        printf("我是孩子他爹/n");  
         count++;  
     }  
-    printf("统计结果是: %d/n",count);  
+    printf("统计结果是: %d\n",count);  
     return 0;  
 }  
