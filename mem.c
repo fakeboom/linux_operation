8e3ee41c1476af1cf78e29f8b8874bdf77691924
fakeboom <fakeboom@foxmail.com>
2020-04-06 07:03:42 +0800

    32

--- mem.c
+++ mem.c
@@ -4,14 +4,14 @@ int main ()
 {   
     char * begin = (char *)malloc(1000000000);
     pid_t fpid; //fpid表示fork函数返回的值  
-    int count=0;  
+    int count=0,i = 0;  
     fpid=fork();   
     if (fpid < 0)   
         printf("error in fork!");   
     else if (fpid == 0) {  
         sleep(60);
         printf("i am the child process, my process id is %d\n",getpid()); 
-        for(int i =0;i<50000000;i++){
+        for(i =0;i<50000000;i++){
             begin[i] = 'a';
         }  
         count++;  
